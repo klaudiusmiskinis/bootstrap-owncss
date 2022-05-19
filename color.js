a4d569032340d7ca72102ca18cac6d100ee2ca0c
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-05-19 10:44:52 +0200

    Updating to Class and instance of it to work it properly on multiple inputs of type color

--- color.js
+++ color.js
@@ -1,12 +1,39 @@
-const primary = document.querySelector("#primaryPicker");
-const initColor = getComputedStyle(document.documentElement).getPropertyValue(
-  "--color"
-);
-primary.value = initColor.trim();
-primary.addEventListener("input", setColor);
+class Picker {
+  name;
+  id;
+  rootId;
+  color;
+  element;
+  initialColor;
 
-function setColor() {
-  document.documentElement.style.setProperty("--color", primary.value);
-  document.querySelector("#primaryColor").innerHTML =
-    "Color primary" + primary.value;
+  constructor(id, rootId, name) {
+    this.id = id;
+    this.rootId = rootId;
+    this.name = name;
+    this.initialize();
+  }
+
+  initialize() {
+    this.element = document.querySelector(this.id);
+    if (this.element) {
+      this.initalColor = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue(this.rootId.trim());
+      this.color = this.initalColor;
+      this.element.value = this.color.trim();
+      this.addEvent();
+    }
+  }
+
+  addEvent() {
+    this.element.addEventListener("input", this.setColor.bind(this));
+  }
+
+  setColor() {
+    document.documentElement.style.setProperty(this.rootId, this.element.value);
+  }
 }
+
+const primary = new Picker("#primaryPicker", "--color");
+const secondary = new Picker("#secondaryPicker", "--color-secondary");
+const third = new Picker("#thirdPicker", "--color-third");
